426718e65f1236bb19384f02163666e753aa7f08
Ragib Hasin <ragibhasin.atwork@gmail.com>
2015-10-22 13:47:20 +0600

    Initial commit

---
+++ Attobyte.Net/HttpWebDownloader.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Attobyte.Net
+{
+    public class HttpWebDownloader
+    {
+        string url, dest, user, pass, cookie;
+        
+        public decimal MaxSpeed
+        {
+            get; set;
+        }
+
+        public decimal Speed
+        {
+            get;
+        }
+
+        long size;
+        public long Size
+        {
+            get { return size; }
+        }
+
+
+    }
+}
